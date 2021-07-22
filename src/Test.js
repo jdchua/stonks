8675ea6515a47d6634c3168658ec8fc1ae7fabfe
Jeremiah Chua <jchua@MBAG-ADV03.MBAG>
2021-07-22 16:39:33 -0700

    add + and - symbols and color changes for stock's current price

--- src/Test.js
+++ src/Test.js
@@ -13,6 +13,7 @@ const CHART_URL = "https://widget.finnhub.io/widgets/stocks/chart?watermarkColor
 const TICKER_URL = "https://finnhub.io/api/v1/search?token=c1lmcqq37fkqle0e1u80";
 const DAILYQUOTE_URL = "https://finnhub.io/api/v1/quote?token=c1lmcqq37fkqle0e1u80";
 const TICKERDATA_URL = "https://finnhub.io/api/v1/stock/candle?resolution=D&token=c1lmcqq37fkqle0e1u80";
+const NEWS_URL = "https://finnhub.io/api/v1/company-news?token=c1lmcqq37fkqle0e1u80"
 
 class Test extends React.Component {
     constructor(props){
@@ -53,22 +54,35 @@ class Test extends React.Component {
         let query = this.state.query;
         axios.get(`${TICKER_URL}&q=${query.replace(/\s/g, "")}`)
         .then(({ data }) => {
+            this.setState({ticker: []});
             this.setState({ticker: this.state.ticker.concat([data.result[0].displaySymbol])}, () => {
                 axios.get(`${DAILYQUOTE_URL}&symbol=${this.state.ticker[this.state.ticker.length - 1]}`)
                 .then(({ data }) => {
                     let dailyQuoteArray = Object.values(data);
+                    this.setState({tickerDailyQuote: []});
                     this.setState({tickerDailyQuote: this.state.tickerDailyQuote.concat(dailyQuoteArray[0].toPrecision(4))});
                 });
                 axios.get(`${TICKERDATA_URL}&symbol=${this.state.ticker[this.state.ticker.length - 1]}&from=${Math.round(Date.now() / 1000) - 864000}&to=${Date.now()}`)
                 .then(({data}) => {
                     let tickerData = Object.entries(data);
+                    this.setState({closingData: []});
+                    this.setState({highData: []});
+                    this.setState({lowData: []});
+                    this.setState({openData: []});
+                    this.setState({dateData: []});
                     this.setState({closingData: [...this.state.closingData, [...tickerData[0][1]]]});
                     this.setState({highData: [...this.state.highData, [...tickerData[1][1]]]});
                     this.setState({lowData: [...this.state.lowData, [...tickerData[2][1]]]});
                     this.setState({openData: [...this.state.openData, [...tickerData[3][1]]]});
                     this.setState({dateData: [...this.state.dateData, [...tickerData[5][1]]]});
-                }) 
+                });
+                axios.get(`${NEWS_URL}&symbol=${this.state.ticker[this.state.ticker.length - 1]}&from=${Math.round(Date.now() / 1000) - 864000}&to=${Date.now()}`)
+                .then(({ data }) => {
+                    console.log(data)                    
+                });
             });
+            this.setState({tickerDescription: []});
+            this.setState({chartUrl: []});
             this.setState({tickerDescription: this.state.tickerDescription.concat(data.result[0].description.split("-", 1))});
             this.setState({chartUrl: this.state.chartUrl.concat(CHART_URL + "&symbol=" + data.result[0].displaySymbol)});
         })
@@ -86,15 +100,18 @@ class Test extends React.Component {
                         <TextField className="searchInput" onChange={this.handleInputChange} inputRef={ref => { this.inputRef = ref; }} id="outlined-basic" label="Search" variant="outlined"/>
                     </form>
                     {this.state.ticker.map((x, index) => (
-                        <div className="main">
+                        <div className="main container">
+                            <div className="row">
+                            <div className="col-md-12">
                             <button>Add To Portfolio [+]</button>
                             <p>{this.state.tickerDescription[index]}</p>
                             <p>${this.state.tickerDailyQuote[index]}</p>
-                            <p>
-                                {this.state.closingData[index] && (this.state.tickerDailyQuote[index] - this.state.closingData[index][this.state.closingData[index].length - 2]).toPrecision(4)} 
-                                <span class="today">Today</span>
-                            </p>
-                            <iframe className="chart" title="Candle chart" width="50%" frameborder="0" height="500" src={this.state.chartUrl[index]}></iframe>
+                            {this.state.closingData[index] && (this.state.tickerDailyQuote[index] - this.state.closingData[index][this.state.closingData[index].length - 2]).toPrecision(4) > 0 && <p className="positive">+{(this.state.tickerDailyQuote[index] - this.state.closingData[index][this.state.closingData[index].length - 2]).toPrecision(4)} Today</p>} 
+                            {this.state.closingData[index] && (this.state.tickerDailyQuote[index] - this.state.closingData[index][this.state.closingData[index].length - 2]).toPrecision(4) < 0 && <p className="negative">{(this.state.tickerDailyQuote[index] - this.state.closingData[index][this.state.closingData[index].length - 2]).toPrecision(4)} Today</p>} 
+                            <iframe className="chart" title="Candle chart" width="83%" frameborder="0" height="500" src={this.state.chartUrl[index]}></iframe>
+                            </div>
+                            <div className="col-md-1"></div>
+                            <div className="col-md-6">
                             <TableContainer className="dataTable" component={Paper}>
                                 <Table size="small" aria-label="a dense table">
                                     <TableHead>
@@ -137,6 +154,13 @@ class Test extends React.Component {
                                     </TableBody>
                                 </Table>
                             </TableContainer>
+                            </div>
+                            <div className="col-md-4">
+                                <p>NEWS</p>
+                            </div>
+                            <div className="col-md-1"></div>
+
+                            </div>
                         </div>
                     ))}
                 </div>
