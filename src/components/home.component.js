bdf95b24d187798c43824016887a1c9dcb592c3e
Jeremiah Chua <jchua@MBAG-ADV03.MBAG>
2021-08-11 16:32:01 -0700

    Add loading spinnner and cascading effect on components

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -11,6 +11,11 @@ import Paper from '@material-ui/core/Paper';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 
+import Grow from '@material-ui/core/Grow';
+
+
+import RingLoader from "react-spinners/RingLoader";
+
 const CHART_URL = "https://widget.finnhub.io/widgets/stocks/chart?watermarkColor=%231db954&amp;backgroundColor=%23222222&amp;textColor=white";
 const TICKER_URL = "https://finnhub.io/api/v1/search?token=c1lmcqq37fkqle0e1u80";
 const DAILYQUOTE_URL = "https://finnhub.io/api/v1/quote?token=c1lmcqq37fkqle0e1u80";
@@ -41,6 +46,7 @@ class Test extends React.Component {
           yearLow:[],
           yearHigh:[],
           dailyVolume: [],
+          loading: false,
           query: ""
         }
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -60,7 +66,10 @@ class Test extends React.Component {
     // - Google Finance
 
     handleSubmit (event) {
+        this.setState({loading: true});
+
         event.preventDefault();
+
         let query = this.state.query;
         axios.get(`${TICKER_URL}&q=${query.replace(/\s/g, "")}`)
         .then(({ data }) => {
@@ -115,6 +124,13 @@ class Test extends React.Component {
             });
             this.setState({chartUrl: []});
             this.setState({chartUrl: this.state.chartUrl.concat(CHART_URL + "&symbol=" + data.result[0].displaySymbol)});
+
+
+            this.setState({loading: false});
+            this.state.loading ? document.getElementsByClassName("main")[0].style.display = "none" : document.getElementsByClassName("main")[0].style.display = "block";
+            setTimeout(() => {
+                this.setState({loading: false});
+            }, 2000);
         })
     }
 
@@ -126,6 +142,7 @@ class Test extends React.Component {
         return (
             <div>
                 <div>
+                    <RingLoader size={50} loading={this.state.loading} />
                     <form class="search" noValidate autoComplete="off" onSubmit={this.handleSubmit}>
                         <TextField className="searchInput" onChange={this.handleInputChange} inputRef={ref => { this.inputRef = ref; }} id="outlined-basic" label="Search" variant="outlined"/>
                     </form>
@@ -133,84 +150,92 @@ class Test extends React.Component {
                         <div className="main container">
                             <div className="row">
                                 <div className="col-md-1"></div>
-                                <div className="col-md-4">
-                                    <button className="addToPortfolio">Add To Portfolio [+]</button>
-                                    <p>{this.state.tickerDescription[index]}</p>
-                                    <p>${this.state.closingData[index] && this.state.closingData[index][this.state.closingData[index].length - 1].toPrecision(4)}</p>
-                                    {this.state.closingData[index] && (this.state.closingData[index][this.state.closingData[index].length - 1].toPrecision(4) - this.state.closingData[index][this.state.closingData[index].length - 2]).toPrecision(4) > 0 && <p className="positive">+{(this.state.closingData[index][this.state.closingData[index].length - 1].toPrecision(4) - this.state.closingData[index][this.state.closingData[index].length - 2]).toPrecision(4)} Today</p>} 
-                                    {this.state.closingData[index] && (this.state.closingData[index][this.state.closingData[index].length - 1].toPrecision(4) - this.state.closingData[index][this.state.closingData[index].length - 2]).toPrecision(4) < 0 && <p className="negative">{(this.state.closingData[index][this.state.closingData[index].length - 1].toPrecision(4) - this.state.closingData[index][this.state.closingData[index].length - 2]).toPrecision(4)} Today</p>} 
-                                    <p>Key Data</p>
-                                    <p>Exchange: {this.state.exchange}</p>
-                                    <p>Day Range: ${this.state.lowData[index] && this.state.lowData[index][this.state.lowData[index].length - 1]} - ${this.state.highData[index] && this.state.highData[index][this.state.highData[index].length - 1]}</p>
-                                    <p>Year Range: ${this.state.yearLow} - ${this.state.yearHigh} </p>
-                                    <p>Volume: {this.state.dailyVolume}</p>
-                                </div>
-                                <div className="col-md-6">
-                                    <iframe className="chart" title="Candle chart" width="100%" frameborder="0" height="500" src={this.state.chartUrl[index]}></iframe>
-                                </div>
+                                <Grow in={true}>
+                                    <div className="col-md-4">
+                                        <button className="addToPortfolio">Add To Portfolio [+]</button>
+                                        <p>{this.state.tickerDescription[index]}</p>
+                                        <p>${this.state.closingData[index] && this.state.closingData[index][this.state.closingData[index].length - 1].toPrecision(4)}</p>
+                                        {this.state.closingData[index] && (this.state.closingData[index][this.state.closingData[index].length - 1].toPrecision(4) - this.state.closingData[index][this.state.closingData[index].length - 2]).toPrecision(4) > 0 && <p className="positive">+{(this.state.closingData[index][this.state.closingData[index].length - 1].toPrecision(4) - this.state.closingData[index][this.state.closingData[index].length - 2]).toPrecision(4)} Today</p>} 
+                                        {this.state.closingData[index] && (this.state.closingData[index][this.state.closingData[index].length - 1].toPrecision(4) - this.state.closingData[index][this.state.closingData[index].length - 2]).toPrecision(4) < 0 && <p className="negative">{(this.state.closingData[index][this.state.closingData[index].length - 1].toPrecision(4) - this.state.closingData[index][this.state.closingData[index].length - 2]).toPrecision(4)} Today</p>} 
+                                        <p>Key Data</p>
+                                        <p>Exchange: {this.state.exchange}</p>
+                                        <p>Day Range: ${this.state.lowData[index] && this.state.lowData[index][this.state.lowData[index].length - 1]} - ${this.state.highData[index] && this.state.highData[index][this.state.highData[index].length - 1]}</p>
+                                        <p>Year Range: ${this.state.yearLow} - ${this.state.yearHigh} </p>
+                                        <p>Volume: {this.state.dailyVolume}</p>
+                                    </div>
+                                </Grow>
+                                <Grow in={true} style={{ transformOrigin: '0 0 0' }} {...(this.state.loading ? { timeout: 500 } : {})}>
+                                    <div className="col-md-6">
+                                        <iframe className="chart" title="Candle chart" width="100%" frameborder="0" height="500" src={this.state.chartUrl[index]}></iframe>
+                                    </div>
+                                </Grow>
                                 <div className="col-md-1"></div>
                             </div>
                             <div className="row">
                                 <div className="col-md-1"></div>
-                                <div className="col-md-4">
-                                        <p> Recent News</p>
-                                        {this.state.news.slice(0, 5).map((x, index) => (
-                                            <div className="col-md-12">
-                                                <a rel="noreferrer" target="_blank" href={x.url}>
-                                                    <p>{x.headline}</p>
-                                                </a>
-                                            </div>
-                                        ))}
-                                </div>
-                                <div className="col-md-6">
-                                <TableContainer className="dataTable" component={Paper}>
-                                    <Table size="small" aria-label="a dense table">
-                                        <TableHead>
-                                        <TableRow>
-                                            <TableCell>Date</TableCell>
-                                            <TableCell>High</TableCell>
-                                            <TableCell>Low</TableCell>
-                                            <TableCell>Open</TableCell>
-                                            <TableCell>Close</TableCell>
-                                        </TableRow>
-                                        </TableHead>
-                                        <TableBody>
+                                <Grow in={true} style={{ transformOrigin: '0 0 0' }} {...(this.state.loading ? { timeout: 1500 } : {})}>
+                                    <div className="col-md-4">
+                                            <p> Recent News</p>
+                                            {this.state.news.slice(0, 4).map((x, index) => (
+                                                <div className="col-md-12">
+                                                    <a rel="noreferrer" target="_blank" href={x.url}>
+                                                        <p>{x.headline}</p>
+                                                    </a>
+                                                </div>
+                                            ))}
+                                    </div>
+                                </Grow>
+                                <Grow in={true} style={{ transformOrigin: '0 0 0' }} {...(this.state.loading ? { timeout: 1000 } : {})}>
+                                    <div className="col-md-6">
+                                    <TableContainer className="dataTable" component={Paper}>
+                                        <Table size="small" aria-label="a dense table">
+                                            <TableHead>
                                             <TableRow>
-                                                <TableCell>
-                                                    {this.state.dateData[index] && this.state.dateData[index].map((x, index) => (
-                                                        <p>{(new Date(x * 1000).toLocaleString("en-US", {month: "numeric", day: "numeric", year: "numeric"}))}</p>
-                                                    ))}
-                                                </TableCell>
-                                                <TableCell>
-                                                    {this.state.highData[index] && this.state.highData[index].map((x, index) => (
-                                                        <p>{x}</p>
-                                                    ))}
-                                                </TableCell>
-                                                <TableCell>
-                                                    {this.state.lowData[index] && this.state.lowData[index].map((x, index) => (
-                                                        <p>{x}</p>
-                                                    ))}
-                                                </TableCell>
-                                                <TableCell>
-                                                    {this.state.openData[index] && this.state.openData[index].map((x, index) => (
-                                                        <p>{x}</p>
-                                                    ))}
-                                                </TableCell>
-                                                <TableCell>
-                                                    {this.state.closingData[index] && this.state.closingData[index].map((x, index, arr) => {
-                                                        const prevItem = arr[index - 1];
-                                                        return (
-                                                            <p className="tableClosingData">
-                                                                {x > prevItem ? <span className="positive"><ArrowDropUpIcon/> {x}</span> : <span className="negative"><ArrowDropDownIcon/> {x.toPrecision(4)}</span>}
-                                                            </p>
-                                                        )
-                                                    })}
-                                                </TableCell>
+                                                <TableCell>Date</TableCell>
+                                                <TableCell>High</TableCell>
+                                                <TableCell>Low</TableCell>
+                                                <TableCell>Open</TableCell>
+                                                <TableCell>Close</TableCell>
                                             </TableRow>
-                                        </TableBody>
-                                    </Table>
-                                </TableContainer>
-                                </div>
+                                            </TableHead>
+                                            <TableBody>
+                                                <TableRow>
+                                                    <TableCell>
+                                                        {this.state.dateData[index] && this.state.dateData[index].map((x, index) => (
+                                                            <p>{(new Date(x * 1000).toLocaleString("en-US", {month: "numeric", day: "numeric", year: "numeric"}))}</p>
+                                                        ))}
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        {this.state.highData[index] && this.state.highData[index].map((x, index) => (
+                                                            <p>{x}</p>
+                                                        ))}
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        {this.state.lowData[index] && this.state.lowData[index].map((x, index) => (
+                                                            <p>{x}</p>
+                                                        ))}
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        {this.state.openData[index] && this.state.openData[index].map((x, index) => (
+                                                            <p>{x}</p>
+                                                        ))}
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        {this.state.closingData[index] && this.state.closingData[index].map((x, index, arr) => {
+                                                            const prevItem = arr[index - 1];
+                                                            return (
+                                                                <p className="tableClosingData">
+                                                                    {x > prevItem ? <span className="positive"><ArrowDropUpIcon/> {x}</span> : <span className="negative"><ArrowDropDownIcon/> {x.toPrecision(4)}</span>}
+                                                                </p>
+                                                            )
+                                                        })}
+                                                    </TableCell>
+                                                </TableRow>
+                                            </TableBody>
+                                        </Table>
+                                    </TableContainer>
+                                    </div>
+                                </Grow>
                                 <div className="col-md-1"></div>
                             </div>
                         </div>
